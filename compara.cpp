0ae889a45bd8ba43d5ac73494869b09ac4b0d09c
KevinQui32 <kevin.caicedo.quinonez@utelvt.edu.ec>
2022-09-29 14:42:35 -0500

    programa compara.cpp modificado

--- compara.cpp
+++ compara.cpp
@@ -1,23 +1,27 @@
 //Modificado por:Kevin Eduardo Caicedo Quiñonez
 //27/09/2022
-#include<iostream> 
- using namespace std; 
- int main() 
- { 
-         float k,c; 
-         cout<<"ingrse k c:"; cin>>k>>c; 
-         if(k>c) 
-         { 
-            cout<<k<<"es mayor k "<<c<<endl; 
-  
-         }else{ 
-             
-            cout<<c<<"es mayor k "<<k<<endl; 
-  
-         } 
-  
-         return 0; 
-  
-  
-  
- }
+#include<iostream>
+using namespace std;
+int main()
+{
+  float k,c;
+  cout<<"Ingrese k  : "; cin>>k;
+  cout<<"Ingrese c  : "; cin>>c;
+  if(k==c){
+     cout<<"Son iguales"<<endl;
+  }else{
+     if(k<c){
+       cout<<k<<" es el menor que "<<c<<"\n";
+     }else{
+       cout<<c<<" es el menor que "<<k<<"\n";
+     }
+}
+
+return (0);;
+
+
+
+
+
+
+}
